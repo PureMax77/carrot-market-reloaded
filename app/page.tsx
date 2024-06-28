2564074318398f674bd27608f494984e16638c47
ertr <ertr@coinplug.com>
2024-06-28 12:18:00 +0900

    3.5 Responsive Modifers

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 export default function Home() {
   return (
     <main className="bg-slate-100 h-screen flex items-center justify-center p-5">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col md:flex-row gap-2">
         <input
           className="w-full rounded-full h-12 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
           type="text"
           placeholder="Search here..."
         />
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10">
           Search
         </button>
       </div>
