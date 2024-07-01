b4766711820e29d752ddd92b68c06aaaeb8f3d95
ertr <ertr@coinplug.com>
2024-07-01 11:26:33 +0900

    3.12 Plugins

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     <main className="bg-slate-100 h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
-        <a href="/">ddd</a>
-        <button className="btn">Submit</button>
+        <input type="text" />
+        <button className="btn text-bi">Submit</button>
       </div>
     </main>
   );
