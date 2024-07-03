aab3f011a9b3a5e88ebb97768bd8f37a964ae860
ertr <ertr@coinplug.com>
2024-07-03 19:39:34 +0900

    5.1 Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,15 +3,27 @@ import FormButton from "@/components/form-button";
 import SocialLogin from "@/components/social-login";
 
 export default function Login() {
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log(123);
+  };
+
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="Email" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="Password"
           required
