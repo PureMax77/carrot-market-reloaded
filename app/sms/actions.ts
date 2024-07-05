858d1244dff9c33ec0676ab746c27761012972db
ertr <ertr@coinplug.com>
2024-07-05 16:04:27 +0900

    6.8 SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -2,9 +2,47 @@
 
 import { z } from "zod";
 import validator from "validator";
+import { redirect } from "next/navigation";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    (phone) => validator.isMobilePhone(phone, "ko-KR"),
+    "잘못된 전화번호입니다."
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prevState: any, formData: FormData) {}
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      // 로그인 성공
+      redirect("/");
+    }
+  }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,9 +5,14 @@ import Button from "@/components/button";
 import { useFormState } from "react-dom";
 import { smsLogin } from "./actions";
 
-export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsLogin, null);
+const initialState = {
+  token: false,
+  error: undefined,
+};
 
+export default function SMSLogin() {
+  const [state, dispatch] = useFormState(smsLogin, initialState);
+  console.log(state);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -15,16 +20,28 @@ export default function SMSLogin() {
         <h2 className="text-xl">Verify tour phone number.</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="text" placeholder="Phone number" required />
-        <Input
-          name="token"
-          type="number"
-          placeholder="Verification code"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="Verify" />
+        {state.token ? (
+          <Input
+            key={1}
+            name="token"
+            type="number"
+            placeholder="Verification code"
+            required
+            min={100000}
+            max={999999}
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <Input
+            key={2}
+            name="phone"
+            type="text"
+            placeholder="Phone number"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "Verify Token" : "Send Verification SMS"} />
       </form>
     </div>
   );
