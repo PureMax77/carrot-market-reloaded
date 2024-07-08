215ff4a88ff549a715d5bfc387efe0b8428888f1
ertr <ertr@coinplug.com>
2024-07-08 09:31:31 +0900

    8.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,20 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
+  const pathname = request.nextUrl.pathname;
+  if (pathname === "/") {
+    const response = NextResponse.next(); // 다음단계(응답)으로 넘어가계하는 트리거 역활
+    response.cookies.set("middleware-cookie", "hello!");
+    return response;
+  }
 
-  if (request.nextUrl.pathname === "/profile") {
+  if (pathname === "/profile") {
     return Response.redirect(new URL("/", request.url));
   }
 }
+
+export const config = {
+  // page 요청 뿐만 아니라 api, static 등등 모든 요청을 통과하므로 아래 것들을 제외하라는 정규식
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
