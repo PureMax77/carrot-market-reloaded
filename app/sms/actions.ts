e6e5c56a03a8a78bfec59bc7ee85246ac34a678d
ertr <ertr@coinplug.com>
2024-07-08 15:49:06 +0900

    9.5 SMS Token

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,8 +1,10 @@
 "use server";
 
+import crypto from "crypto";
 import { z } from "zod";
 import validator from "validator";
 import { redirect } from "next/navigation";
+import db from "@/lib/db";
 
 const phoneSchema = z
   .string()
@@ -18,6 +20,24 @@ interface ActionState {
   token: boolean;
 }
 
+async function getToken() {
+  const token = crypto.randomInt(100000, 999999).toString();
+  const exists = await db.sMSToken.findUnique({
+    where: {
+      token,
+    },
+    select: {
+      id: true,
+    },
+  });
+  if (exists) {
+    // 생성한 토큰과 같은 토큰이 이미 존재하면 다시 회귀
+    return getToken();
+  } else {
+    return token;
+  }
+}
+
 export async function smsLogin(prevState: ActionState, formData: FormData) {
   const phone = formData.get("phone");
   const token = formData.get("token");
@@ -29,6 +49,33 @@ export async function smsLogin(prevState: ActionState, formData: FormData) {
         error: result.error.flatten(),
       };
     } else {
+      // 폰번호 유효성 통과 후
+      await db.sMSToken.deleteMany({
+        where: {
+          user: {
+            phone: result.data,
+          },
+        },
+      });
+      const token = await getToken();
+      await db.sMSToken.create({
+        data: {
+          token,
+          user: {
+            // 사용자가 없으면 사용자를 만들고 연결
+            connectOrCreate: {
+              where: {
+                phone: result.data,
+              },
+              create: {
+                username: crypto.randomBytes(10).toString("hex"),
+                phone: result.data,
+              },
+            },
+          },
+        },
+      });
+
       return {
         token: true,
       };
