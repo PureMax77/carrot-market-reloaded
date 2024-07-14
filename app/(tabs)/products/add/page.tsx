2c473892130824144d9e15ce4db1ee705b4e769b
ertr <ertr@coinplug.com>
2024-07-15 08:59:48 +0900

    11.1 Form Action

--- app/(tabs)/products/add/page.tsx
+++ app/(tabs)/products/add/page.tsx
@@ -4,21 +4,42 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { uploadProduct } from "./actions";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
 
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) {
+      return;
+    }
+    const file = files[0];
+    // 브라우저에 올라간 이미지 메모리 주소URL
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
 
   return (
     <div>
-      <form action="" className="p-5 flex flex-col gap-5">
+      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
-          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer"
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neutral-400 text-sm">사진을 추가해주세요.</div>
+          {!preview && (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-400 text-sm">
+                사진을 추가해주세요.
+              </div>
+            </>
+          )}
         </label>
         {/* label을 눌러도 input이 눌러지니까 숨김 */}
         <input
